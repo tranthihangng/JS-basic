ddd237ff0982dea751f59c6dcdb2d96e43a85c1d
tranthihangng <hangygd@gmail.com>
2024-08-31 17:56:27 +0700

    upload den vd7

---
+++ video6/video6.js
@@ -0,0 +1,6 @@
+console.log("hello hang")
+let a= 5
+let b = 9
+//url: /hangtran/5
+console.log(`check string:a = ${a} b = ${b} 
+    tong la: ${a+b}` )
---
+++ video7_object/video7.js
@@ -0,0 +1,16 @@
+let a = {
+    name: 'hang',
+    address: "vinh phuc",
+   
+}
+let b = 'hang'
+let c = ' '
+console.log('type of a: ', typeof a,
+     ' type of b:', typeof b,
+    'type of c:', typeof c)
+let k = 'name'
+a.name = 'HCM'
+
+console.log('my name is: ',a.name)
+console.log('my name is: ',a['name'], a)
+
