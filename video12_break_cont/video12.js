7256b9e3a872b1ffa30457b2d10597def138dd42
tranthihangng <hangygd@gmail.com>
2024-08-31 23:41:45 +0700

    chapter lan 2

---
+++ video12_break_cont/video12.js
@@ -0,0 +1,12 @@
+let arr = ['orange', 'apple','banana','na','ba']
+let i = 0
+while(i<arr.length){
+    // console.log('fruit top: ', i+1, arr[i])
+    // if(arr[i].length<3)
+    //     console.log(arr[i])
+    i++
+    if (arr[i]==='apple')
+        continue;
+    console.log('kq: ', arr[i])
+    
+}
---
+++ video13_func/video13.js
@@ -0,0 +1,16 @@
+
+
+// function sum(a, b){
+//     return a+b;
+// }
+// console.log(sum(9,6))
+
+function getName(x,y){
+    return x+y
+}
+console.log('hangtr')
+//arrow function
+let sum = (a,b)=>{
+    return a+b;
+}
+console.log('check sum: ', sum(5,8))
