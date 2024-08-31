0f166142cb0ad6c81ca8acb15c247be072b25965
tranthihangng <hangygd@gmail.com>
2024-08-31 16:36:47 +0700

    upload vd5

---
+++ video5/video5.js
@@ -0,0 +1,11 @@
+console.log('hello Hang '.toUpperCase())
+let a = 5
+let b = 6.8
+let d = '5'
+// let e = Number('d')
+let e = +d
+// console.log(a+d)
+// console.log(a*d)
+console.log(a===d,'a: ', typeof a,'e: ', typeof e)
+
+
